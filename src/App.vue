6e412e01ff0bc2883e4a6f31d6c422104a20a858
joonsol <lizlove0523@gmail.com>
2022-04-06 18:25:30 +0900

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header></Header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Header from './components/Header'
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-size: 62.5%;
+
+  }
+
+  a {
+    text-decoration: none;
+    color: #2c3e50;
+    font-size: 1.4rem;
+  }
+
+  #container {
+    text-align: center;
+    margin: 0 20px;
+  }
+
+  h2 {
+    margin-bottom: 20px;
+    font-size: 30px;
+  }
+
+  img {
+    width: 200px;
+    height: auto;
+  }
+
+  h3 {
+    color: gray;
+    font-size: 1.6rem;
+    margin: 3rem 0 1rem;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    header
+  </header>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+  }
+
+  h1 a {
+    font-size: 3rem;
+    text-transform: uppercase;
+  }
+
+  nav a {
+    padding: 0 2rem;
+  }
+
+  nav a.router-link-active {
+    color: blueviolet
+  }
+</style>
